e850a9e836b38f14243fa4e4530f9388acd27fe8
Duc Cao <caovanducs@gmail.com>
2020-11-28 16:02:53 +0700

    Customing theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Components/Login/Login";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const custom_theme = createMuiTheme({
+  palette: {
+    primary: {
+      //light: "red",
+      //  dark: "red",
+      main: "#489DEB",
+      contrastText: "white",
+    },
+  },
+  props: {
+    MuiOutlinedInput: {
+      color: "primary",
+    },
+
+    MuiInputBase: {
+      color: "red;",
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={custom_theme}>
       <Login />
-    </div>
+    </ThemeProvider>
   );
 }
 
